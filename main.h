195be0d3052894c3700b55ddcc393047a82af35e
Mutegeki Ronald <murogive@gmail.com>
2022-08-07 00:14:52 -0700

    Added main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*Include standard C libraries*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif
