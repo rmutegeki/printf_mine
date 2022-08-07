76fc1f714283f3575ec7b7f299a21a2a20428884
Mutegeki Ronald <murogive@gmail.com>
2022-08-07 00:14:16 -0700

    _Added printf.c

---
+++ _printf.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _printf - variadic function that formats and prints data.
+ * @format: the format string
+ *
+ * Return: the number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list arg_list;
+	unsigned int i = 0, chars_written = 0;
+
+	int (*temp_func)(va_list), no_conversion;
+
+
+	if (!format)
+
+		return (0);
+
+
+	va_start(arg_list, format);
+
+	while (format[i])
+
+	{
+
+		no_conversion = 1;
+
+		if (format[i] == '%')
+
+		{
+
+			temp_func = match_specifier(format[++i]);
+
+			if (temp_func)
+
+			{
+
+				chars_written += temp_func(arg_list);
+
+				i++, no_conversion = 0;
+
+			}
+
+		}
+
+		if (no_conversion)
+
+		{
+
+			_putchar(format[i++]);
+
+			chars_written++;
+
+		}
+
+	}
+
+	va_end(arg_list);
+
+	return (chars_written);
+
+}
